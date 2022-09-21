6e3d4e3bc31fea43856110333d881b9bf862e1ad
Shruthi-S1302 <89682079+Shruthi-S1302@users.noreply.github.com>
2022-09-21 23:49:00 +0530

    First-commit

---
+++ src/Components/KnowMeStyle.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+export const KnowMeStyle = styled.div`
+  h2 {
+    color: #000;
+    font-family: "Lexend Deca", sans-serif;
+    text-align: center;
+    margin-left: 6%;
+    padding-top: 5%;
+  }
+  h2:after {
+    border-bottom: 2px solid black;
+    display: block;
+    margin-left: 48%;
+    content: " ";
+    width: 50px;
+  }
+  p {
+    /* text-align: center;
+    margin: 2% 5% 5% 5%;
+    line-height: 2.5; */
+    margin: 5%;
+    line-height: 2;
+    color: ;
+    font-family: "Lexend Deca", sans-serif;
+    font-size: 1.25rem;
+  }
+`;
