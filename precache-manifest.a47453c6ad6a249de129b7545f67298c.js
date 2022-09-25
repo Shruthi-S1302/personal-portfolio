d86be5ecb6f2820bb9e1e04943c3479b38faf0cd
Shruthi-S1302 <89682079+Shruthi-S1302@users.noreply.github.com>
2022-09-26 00:04:32 +0530

    Updates

---
+++ precache-manifest.a47453c6ad6a249de129b7545f67298c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ee7d6939872d915279",
+    "url": "/personal-portfolio/static/css/main.bf30e21f.chunk.css"
+  },
+  {
+    "revision": "90ee7d6939872d915279",
+    "url": "/personal-portfolio/static/js/main.90ee7d69.chunk.js"
+  },
+  {
+    "revision": "ba9fd9f92c5cae8058d7",
+    "url": "/personal-portfolio/static/js/1.ba9fd9f9.chunk.js"
+  },
+  {
+    "revision": "fc41262401350ab4c027",
+    "url": "/personal-portfolio/static/js/runtime~main.fc412624.js"
+  },
+  {
+    "revision": "def9f52efe3806601d6f369a0663a17e",
+    "url": "/personal-portfolio/static/media/programming.def9f52e.png"
+  },
+  {
+    "revision": "bf7e1585dda197df7bb08dcee2d1c22e",
+    "url": "/personal-portfolio/static/media/Resume.bf7e1585.pdf"
+  },
+  {
+    "revision": "7bc1ea31190b0d6371c90ada0cbef7e9",
+    "url": "/personal-portfolio/static/media/Profile.7bc1ea31.jpg"
+  },
+  {
+    "revision": "9ee6cf7afc1d282ca4072a7c54085669",
+    "url": "/personal-portfolio/static/media/coffee.9ee6cf7a.png"
+  },
+  {
+    "revision": "f6e8ff6f0fbc8dccff4bf86f52de2d82",
+    "url": "/personal-portfolio/static/media/frontend.f6e8ff6f.png"
+  },
+  {
+    "revision": "d76ea2971e4e62d4aa64bc3a96f9375c",
+    "url": "/personal-portfolio/static/media/website.d76ea297.png"
+  },
+  {
+    "revision": "4df14e8e3da390b697c40c79a9d87463",
+    "url": "/personal-portfolio/static/media/bg.4df14e8e.jpg"
+  },
+  {
+    "revision": "2eca9637d29115dc5fc367cf16a5ce77",
+    "url": "/personal-portfolio/index.html"
+  }
+];
